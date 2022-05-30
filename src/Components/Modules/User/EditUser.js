eaed83ecde5f0b312d87354ba451069a2094ecff
Hana Jiffry <hanajiffryscg@gmail.com>
2022-05-30 19:52:20 +0530

    Adding uses States

--- src/Components/Modules/User/EditUser.js
+++ src/Components/Modules/User/EditUser.js
@@ -10,9 +10,20 @@ import {
   Button1,
   Button2
 } from './UserStyles';
-
+import { useState } from "react";
 
 const EditUser = () => {
+
+  const[name,setName]=useState("");
+  const[email,setEmail]=useState("");
+  const[password,setPassword]=useState("");
+  const[position,setPosition]=useState("");
+  const[role,setRole]=useState("");
+
+  const displayinfo = () =>{
+    console.log(name+email+password+position+role);
+  }
+
   return(
     <>
     <Container>
@@ -27,30 +38,56 @@ const EditUser = () => {
        <Form>
         <Form.Group controlId="form.Name">
             <Form.Label><ContainerText >User Name</ContainerText></Form.Label>
-            <Form.Control as="textarea" rows={1} />
+            <Form.Control as="textarea" rows={1} 
+            type='text' 
+            onChange={(event)=>{
+              setName(event.target.value);
+            }}/>
         </Form.Group><br/>
+
         <Form.Group controlId="form.Email">
             <Form.Label><ContainerText>User Email</ContainerText></Form.Label>
-            <Form.Control as="textarea" rows={1} />
+            <Form.Control as="textarea" rows={1} 
+            type='text' 
+            onChange={(event)=>{
+              setEmail(event.target.value);
+            }}/>
         </Form.Group><br/>
+
         <Form.Group controlId="form.Email">
             <Form.Label><ContainerText>Password</ContainerText></Form.Label>
-            <Form.Control as="textarea" rows={1} />
+            <Form.Control as="textarea" rows={1} 
+            type='text' 
+            onChange={(event)=>{
+              setPassword(event.target.value);
+            }}/>
         </Form.Group><br/>
+
         <Form.Group controlId="form.Textarea">
             <Form.Label><ContainerText>Designation</ContainerText></Form.Label>
-            <Form.Control as="textarea" rows={1} />
+            <Form.Control as="textarea" rows={1} 
+            type='text' 
+            onChange={(event)=>{
+              setPosition(event.target.value);
+            }}
+            />
         </Form.Group><br/>
+
         <Form.Group controlId="form.Textarea">
             <Form.Label><ContainerText>Role</ContainerText></Form.Label>
-            <Form.Control as="textarea" rows={1} />
+            <Form.Control as="textarea" rows={1} 
+            type='text' 
+            onChange={(event)=>{
+              setRole(event.target.value);
+            }}
+            />
         </Form.Group>
       </Form>
        </InputBox>
 
       <br/>
     
-      <Button1 size="sm">Update</Button1>
+      <Button1 size="sm" onClick={displayinfo}>Update</Button1>
       <Button2 size="sm">Discard</Button2>
 
     </ContainerBox>
